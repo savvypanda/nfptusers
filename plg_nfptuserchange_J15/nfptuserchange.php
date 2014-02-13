1aec48a7ad69dc9b64a2869a1405ba148ca117fa
Levi Carter <levi@savvypanda.com>
2014-02-13 16:12:14 -0500

    Initial Commit

---
+++ plg_nfptuserchange_J15/nfptuserchange.php
@@ -0,0 +1,346 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+
+class plgUsernfptuserchange extends JPlugin {
+	private $nfpt_db;
+	private $olduserinfo;
+
+	//We need to use the beforeSave method to grab the user's existing email address
+	function onBeforeStoreUser($olduser, $isnew) {
+		global $nfptuserpluginflag;
+
+		//if the user was added by way of the nfptusersync system plugin, we do not want to fire this plugin since that will cause an endless loop
+		if(isset($nfptuserpluginflag) && $nfptuserpluginflag === true) {
+			return true;
+		}
+
+		//if we cannot connect to the database we can't continue
+		if(!$this->db_connect($olduser['email'])) {
+			JFactory::getApplication()->redirect(JRequest::getString('HTTP_REFERRER', '', 'SERVER'), 'Unable to connect to users database. Please try again later, or contact the website administrator for assistance.');
+			return false;
+		}
+
+		//we need to have both the old user information and the new user information available
+		$this->olduserinfo = array();
+		$newemail = $olduser['email'];
+		$newusername = $olduser['username'];
+		if(($isnew && isset($_POST['email'])) || (isset($_POST['id']) && $_POST['id'] == $olduser['id'] && isset($_POST['email']))) {
+			$newemail = $_POST['email'];
+		}
+		if(($isnew && isset($_POST['username'])) || (isset($_POST['id']) && $_POST['id'] == $olduser['id'] && isset($_POST['username']))) {
+			$newusername = $_POST['username'];
+		}
+
+		//if we are updating the user, we want to save the old email so that we can change it properly in the onUserAfterSave event
+		if(!$isnew) {
+			$this->olduserinfo['email']=$olduser['email'];
+		}
+
+		//before we allow the user to be saved in the database, we need to make sure that we will not be inserting a duplicate email address into the database
+		if($isnew || $newemail != $olduser['email']) {
+			$query = 'SELECT count(*) FROM users_live WHERE email='.$this->nfpt_db->quote($newemail);
+			$this->nfpt_db->setQuery($query);
+			$this->nfpt_db->query();
+			if($this->nfpt_db->loadResult() != 0) {
+				JFactory::getApplication()->redirect(JRequest::getString('HTTP_REFERRER', '', 'SERVER'), 'A user with that email address already exists in our user database. Please use a different email address or contact an administrator for assistance.');
+				return false;
+			}
+		}
+		//we also don't want to insert duplicate usernames since those are supposed to be unique
+		if($isnew || $newusername != $olduser['username']) {
+			$query = 'SELECT count(*) FROM users_live WHERE username='.$this->nfpt_db->quote($newusername);
+			$this->nfpt_db->setQuery($query);
+			$this->nfpt_db->query();
+			if($this->nfpt_db->loadResult() != 0) {
+				JFactory::getApplication()->redirect(JRequest::getString('HTTP_REFERRER', '', 'SERVER'), 'A user with that username address already exists in our user database. Please use a different username or contact an administrator for assistance.');
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Store User Method
+	 * @param 	array		holds the new user data
+	 * @param 	boolean		true if a new user is stored
+	 * @param	boolean		true if user was succesfully stored in the database
+	 * @param	string		message
+	 */
+	function onAfterStoreUser($user, $isnew, $success, $msg) { //J1.5 method name
+		global $nfptuserpluginflag;
+
+		//if the user was added by way of the nfptusersync system plugin, we do not want to fire this plugin since that will cause an endless loop
+		if(isset($nfptuserpluginflag) && $nfptuserpluginflag === true) {
+			return true;
+		}
+		//if we were not able to connect to the database, we cannot procede
+		if(!$this->db_connect($user['email'])) {
+			return false;
+		}
+		//if the user was not successfully stored in the database. We don't want to update the users_live table with bad information
+		if(!$success) {
+			return false;
+		}
+
+		//this code is necessary to handle when a user changes
+		$new_email = $nfpt_email = $user['email'];
+		$email_change = false;
+		if(!$isnew && !empty($this->olduserinfo['email'])) {
+			$nfpt_email = $this->olduserinfo['email'];
+			if($nfpt_email != $new_email) {
+				$email_change = true;
+			}
+		}
+
+		//check to see how many users exist in the users_live table with this email address
+		$query = 'SELECT * FROM users_live WHERE email='.$this->nfpt_db->quote($nfpt_email);
+		$this->nfpt_db->setQuery($query);
+		$this->nfpt_db->query();
+		$existing_users = $this->nfpt_db->getNumRows();
+
+		//if there are more than one users in the users_live table with this email address, we do not know which user to update
+		if($existing_users > 1) {
+			$message = 'There are multiple users in the users_live table with this email. Remove the duplicates and try updating the user again.';
+			if($email_change) {
+				$message .= " (special note - the user's email address was changed to '$new_email')";
+			}
+			$this->addLog($nfpt_email, 'UPDATE', 'ERROR', $message);
+			return false;
+		}
+
+		//******************************************************************************
+		//***************** Get User Information ***************************************
+		$myfield = 'update'.$this->params->get('myupdatefield');
+		$userfields = array(
+			'name' => $user['name'],
+			'email' => $new_email,
+			'username' => $user['username'],
+			'blocked' => $user['block'],
+			'deleted' => 0,
+			$myfield => 0,
+		);
+		if($email_change) {
+			$userfields['previousemail'] = $nfpt_email;
+		}
+
+		$query = 'SHOW COLUMNS FROM users_live';
+		$this->nfpt_db->setQuery($query);
+		$allcolumns = $this->nfpt_db->loadResultArray();
+		foreach($allcolumns as $col) {
+			if(substr($col,0,6)=='update' && $col != $myfield) {
+				$userfields[$col] = 1;
+			}
+		}
+
+		//get the user's plaintext password, if possible.
+		//also get the user's encrypted password.
+		$password = false;
+		$possible_passfields = array(
+			$user['password_clear'],
+			$_POST['password'],
+			$_POST['password1'],
+			$_POST['password2'],
+			$_POST['jspassword'],
+			$user['password2'],
+		);
+		foreach($possible_passfields as $pass) {
+			if(!$password && !empty($pass)) {
+				$password = $pass;
+			}
+		}
+		if($password) {
+			$userfields['passplain'] = $password;
+			$userfields['passjoom'] = $user['password'];
+		}
+
+		//******************************************************************************
+		//***************** Insert or Update User **************************************
+		if($existing_users == 0) {
+			//Create GUID for user
+			mt_srand((double) microtime() * 10000);
+			$charid = strtoupper(md5(uniqid(rand(), true)));
+			$userfields['user_guid'] = substr($charid,  0, 8).'-'.substr($charid,  8, 4).'-'.substr($charid, 12, 4).'-'.substr($charid, 16, 4).'-'.substr($charid, 20, 12);
+
+			//Create and run the INSERT query
+			$fields = array();
+			$values = array();
+			foreach($userfields as $field => $value) {
+				$fields[] = $field;
+				$values[] = $this->nfpt_db->quote($value);
+			}
+			$query = 'INSERT INTO users_live('.implode(',',$fields).') VALUES('.implode(',',$values).')';
+			$this->nfpt_db->setQuery($query);
+			$this->nfpt_db->query();
+			if($this->nfpt_db->getErrorNum()) {
+				//we failed to insert the user, log the error in the userlog table
+				$this->addLog($nfpt_email, 'INSERT', 'ERROR', 'Error when running database insert query: '.$this->nfpt_db->getErrorMsg());
+				return false;
+			} else {
+				//we successfully inserted the user, log the success
+				$this->addLog($nfpt_email, 'INSERT');
+				return true;
+			}
+		} else if($existing_users == 1) {
+			//Create and run the UPDATE query
+			$fields = array();
+			foreach($userfields as $field => $value) {
+				$fields[] = $field.'='.$this->nfpt_db->quote($value);
+			}
+			$query = 'UPDATE users_live SET '.implode(', ',$fields).' WHERE email='.$this->nfpt_db->quote($nfpt_email);
+			$this->nfpt_db->setQuery($query);
+			$this->nfpt_db->query();
+			if($this->nfpt_db->getErrorNum()) {
+				//we failed to update the user, log the error in the userlog table
+				$this->addLog($nfpt_email, 'UPDATE', 'ERROR', 'Error when running database update query: '.$this->nfpt_db->getErrorMsg());
+				return false;
+			}
+			if(($numrows = $this->nfpt_db->getAffectedRows()) != 1) {
+				if($numrows==0) {
+					//we failed to update the user, log it as an error
+					$this->addLog($nfpt_email, 'UPDATE', 'ERROR', 'No rows affected by update query. Something isn\'t right here.');
+					return false;
+				} else {
+					//we updated more than 1 user, log it as an error
+					$this->addLog($nfpt_email, 'UPDATE', 'ERROR', $numrows.' rows affected by update query. Should be 1. Something isn\'t right here.');
+					return false;
+				}
+			} else {
+				//we successfully updated the user, log the success
+				$this->addLog($nfpt_email, 'UPDATE');
+				return true;
+			}
+		}
+
+		//we should never get here. The function should always have returned a value in one of the previous if statements
+		JError::raiseError('500','User add/update function failed to return value. Contact your administrator for assistance.');
+		return false;
+	}
+
+	//We need a way to delete users, too!
+	function onAfterDeleteUser($user, $success, $msg) {
+		global $nfptuserpluginflag;
+
+		//if we are calling the delete from the system plugin, we do not want to run this function
+		if(isset($nfptuserpluginflag) && $nfptuserpluginflag===true) {
+			return true;
+		}
+
+		//if we were not able to connect to the database, we cannot procede
+		if(!$this->db_connect($user['email'])) {
+			return false;
+		}
+		//if the user was not successfully deleted. We don't want to delete from in the users_live table
+		if(!$success) {
+			return false;
+		}
+
+		$myfield = 'update'.$this->params->get('myupdatefield');
+		$updates = array('deleted=\'1\'', $myfield.'=\'0\'');
+
+		$query = 'SHOW COLUMNS FROM users_live';
+		$this->nfpt_db->setQuery($query);
+		$allcolumns = $this->nfpt_db->loadResultArray();
+		foreach($allcolumns as $col) {
+			if(substr($col,0,6)=='update' && $col != $myfield) {
+				$updates[] = $col.'=\'1\'';
+			}
+		}
+
+		$query='UPDATE users_live SET '.implode(', ',$updates).' WHERE email='.$this->nfpt_db->quote($user['email']);
+		$this->nfpt_db->setQuery($query);
+		$this->nfpt_db->query();
+		if(($numrows = $this->nfpt_db->getAffectedRows()) != 1) {
+			if($numrows==0) {
+				//we failed to delete the user in the users_live table, log it as an error
+				$this->addLog($user['email'], 'DELETE', 'ERROR', 'Unable to mark the user in the users_live table for deletion. No rows affected by update query.');
+				return false;
+			} else {
+				//we deleted more than 1 user, log it as an error
+				$this->addLog($user['email'], 'DELETE', 'ERROR', 'Accidentally marked '.$numrows.' rows in the user_live table for deletion. Should be 1. Something isn\'t right here.');
+				return false;
+			}
+		} else {
+			//we successfully updated the user, log the success
+			$this->addLog($user['email'], 'DELETE');
+			return true;
+		}
+	}
+
+	private function db_connect($emailval) {
+		//if we have already connected, we do not need to connect again
+		if($this->nfpt_db) return true;
+
+		//otherwise, connect using the information in the plugin parameters
+		$nfpt_options = array(
+			'host' => $this->params->get('mainhost'),
+			'user' => $this->params->get('mainuser'),
+			'password' => $this->params->get('mainpass'),
+			'database' => $this->params->get('maindb'),
+		);
+		$this->nfpt_db = JDatabase::getInstance($nfpt_options);
+
+		//if we were unable to connect to the database, send an email to the administrator notifying them.
+		//if the email fails to send, at least we can notify the user that there was a problem.
+		if($this->nfpt_db instanceof JException || $this->nfpt_db->getErrorNum()) {
+			$this->nfpt_db = false;
+			$mailer = JFactory::getMailer();
+			$config = JFactory::getConfig();
+			$site = JURI::root();
+			$message = <<<HEREDOC
+Failed to connect to the users database from $site after saving user.
+The user's email address is '$emailval'.
+
+The easiest way to correct this user's information in the database:
+1. go the administrator interface on $site
+2. search for '$emailval' from the users manager
+3. edit the user with that email address.
+4. press the "Save and Close" button. You do not actually have to change any values.
+
+If you repeately receive this email from $site, that is an indicator that the database connection is no longer working from there. Please change the database connection details in the user plugin, or contact tech@mangotreemedia.com for assistance.
+HEREDOC;
+			$mailer->addRecipient($this->params->get('failemail'));
+			$mailer->setSender($config->getValue('config.mailfrom'), $config->getValue('config.fromname'));
+			$mailer->setSubject('User Save Failure');
+			$mailer->setBody($message);
+			if(!$mailer->Send()) {
+				JError::raiseError(500, 'Unable to connect to user database. Please contact tech@mangotreemedia.com for assistance.');
+			}
+			//since we were unable to connect to the database, return false for failure.
+			return false;
+		}
+
+		//We were able to initiate the database connection. Return true for success.
+		return true;
+	}
+
+	private function addlog($email, $operation, $status='SUCCESS', $notes = '') {
+		$origin=JURI::base();
+		$query = 'INSERT INTO userlog(origin, email, operation, status, notes) VALUES ('.$this->nfpt_db->quote($origin).','.$this->nfpt_db->quote($email).','.$this->nfpt_db->quote($operation).','.$this->nfpt_db->quote($status).','.$this->nfpt_db->quote($notes).')';
+		$this->nfpt_db->setQuery($query);
+		$this->nfpt_db->query();
+		if($this->nfpt_db->getErrorNum()) {
+			$mailer = JFactory::getMailer();
+			$config = JFactory::getConfig();
+			$site = JURI::root();
+			$message = <<<HEREDOC
+Failed to insert userlog entry into the database from $site after processing user.
+The user's email address is '$email'.
+The attempted operation was '$operation', which terminiated with status '$status'.
+
+NOTES (if applicable):
+$notes
+
+If you receive this email, there is something fishy going on - please contact tech@mangotreemedia.com for assistance.
+HEREDOC;
+			$mailer->addRecipient($this->params->get('failemail'));
+			$mailer->setSender($config->getValue('config.mailfrom'), $config->getValue('config.fromname'));
+			$mailer->setSubject('Userlog Failure');
+			$mailer->setBody($message);
+			if(!$mailer->Send()) {
+				JError::raiseWarning(400,'Unable to save information to our other sites. Please contact tech@mangotreemedia.com for assistance.');
+			}
+		}
+	}
+}
+?>
---
+++ plg_nfptusersync_J15/nfptusersync.php
@@ -0,0 +1,234 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+
+class plgSystemnfptusersync extends JPlugin {
+	private $nfpt_db;
+	/*
+	 * This is where the plugin does all the work.
+	 */
+	function onAfterInitialise() {
+		global $nfptuserpluginflag;
+		$app = JFactory::getApplication();
+		if($app->isAdmin()) {
+			//do not run from administrator interface
+			return true;
+		}
+		//temporary measure to ignore this plugin entirely for all users except for me - for testing purposes
+		//if(JRequest::getString('10skdOlsid03kDKowl','false') != 'aodkfnxog20498KSL') {
+		//	return;
+		//}
+
+		//start by connecting to the users_live database
+		if(!$this->db_connect()) {
+			if(JRequest::getString('10skdOlsid03kDKowl','false') != 'aodkfnxog20498KSL') {
+				die('UNABLE TO CONNECT TO USERS LIVE DATABASE. SYNCING USERS FAILED.');
+			}
+			return false;
+		}
+
+		//From here on out, we want to hide warnings and notices from users if they happen. Use our own error handler instead of the regular Joomla error handlers
+		JError::setErrorHandling(E_WARNING, 'callback', array(get_class($this), 'handleWarning'));
+		JError::setErrorHandling(E_NOTICE, 'callback', array(get_class($this), 'handleNotice'));
+
+		//Now that we have a database connection, fetch the users to process
+		$myfield = 'update'.$this->params->get('myupdatefield');
+		$query = "SELECT * FROM users_live WHERE $myfield=1";
+		$this->nfpt_db->setQuery($query);
+		$this->nfpt_db->query();
+		$updateusers = $this->nfpt_db->loadObjectList();
+
+		if(!empty($updateusers)) {
+			jimport('joomla.application.component.helper');
+			$users_config = JComponentHelper::getParams('com_users');
+			$defaultUserGroup = $users_config->get('new_usertype','Registered');
+			$acl = JFactory::getACL();
+			$defaultGroupId = $acl->get_group_id('',$defaultUserGroup,'ARO');
+
+			foreach($updateusers as $nfpt_user) {
+				//check to see if the user already exists in the database
+				$findfields = array();
+				$findfields[] = array('key'=>'email', 'value'=>$nfpt_user->email); //the new/updated email should always be first
+				if(!empty($nfpt_user->previousemail)) {
+					//if the email address has recently changed, that should be the second search field
+					$findfields[] = array('key'=>'email', 'value'=>$nfpt_user->previousemail);
+				}
+				$findfields[] = array('key'=>'username', 'value'=>$nfpt_user->username); //the username should also be a unique identifier
+				$user = $this->fetch_juser_from_fields($findfields);
+				$success = false;
+
+				//*******************************************************
+				//CASE 1. If the user is marked for deletion, delete them
+				if($nfpt_user->deleted) {
+					if(!$user) {
+						//we cannot delete a user that does not already exist. Log this in the userlog table
+						$this->addlog($nfpt_user->email, 'SYNC', 'IGNORED', 'Could not locate the user to delete. Skipping.');
+						$success = true;
+					} else {
+						//necessary helper variable for preventing the nfptuserchange plugin from firing when we save the user
+						$nfptuserpluginflag = true;
+						if($user->delete()) {
+							$success = true;
+						} else {
+							$this->addlog($nfpt_user->email, 'SYNC', 'ERROR', 'Could not delete user. Please contact tech@mangotreemedia.com for assistance.');
+						}
+						$nfptuserpluginflag = false;
+					}
+				//***************************************
+				//CASE 2. If this is a new user, add them
+				} else if(!$user)  {
+					$data = array(
+						'name' => $nfpt_user->name,
+						'username' => $nfpt_user->username,
+						'usertype' => $defaultUserGroup,
+						'email' => $nfpt_user->email,
+						'gid' => $defaultGroupId,
+						'password' => $nfpt_user->passplain,
+						'password2' => $nfpt_user->passplain,
+						'block' => $nfpt_user->blocked,
+					);
+
+					//necessary helper variable for preventing the nfptuserchange plugin from firing when we save the user
+					$nfptuserpluginflag = true;
+					$newuser = new JUser;
+					if($newuser->bind($data) && $newuser->save()) {
+						$success = true;
+					} else {
+						$this->addlog($nfpt_user->email, 'SYNC', 'ERROR', 'Could not add user. Error: '.$newuser->getError());
+					}
+					$nfptuserpluginflag = false;
+				//***************************************
+				//CASE 3. If this is an existing user, udpate them
+				} else {
+					$data = array(
+						'name' => $nfpt_user->name,
+						'username' => $nfpt_user->username,
+						'email' => $nfpt_user->email,
+						'password' => $nfpt_user->passplain,
+						'password2' => $nfpt_user->passplain,
+						'block' => $nfpt_user->blocked,
+					);
+					//necessary helper variable for preventing the nfptuserchange plugin from firing when we save the user
+					$nfptuserpluginflag = true;
+					if($user->bind($data) && $user->save()) {
+						$success = true;
+					} else {
+						$this->addlog($nfpt_user->email, 'SYNC', 'ERROR', 'Could not update user. Error: '.$user->getError());
+					}
+					$nfptuserpluginflag = false;
+
+				}
+
+				//**************************************************************************************//
+				//************** Finally, remove our update flag from the users_live table *************//
+				if($success) {
+					$emptyemail = true;
+					//if there is a previousemail field but no more databases to update, we can remove the previous email from the record
+					if(!empty($nfpt_user->previousemail)) {
+						$nfpt_vars = get_object_vars($nfpt_user);
+						foreach($nfpt_vars as $var => $val) {
+							if(substr($var,0,6)=='update' && $var != $myfield && $val == 1) {
+								$emptyemail = false;
+							}
+						}
+					}
+					if($emptyemail) {
+						$query = "UPDATE users_live SET $myfield=0, previousemail='' WHERE email=".$this->nfpt_db->quote($nfpt_user->email);
+					} else {
+						$query = "UPDATE users_live SET $myfield=0 WHERE email=".$this->nfpt_db->quote($nfpt_user->email);
+					}
+					$this->nfpt_db->setQuery($query);
+					$this->nfpt_db->query();
+					if(($numrows = $this->nfpt_db->getAffectedRows()) != 1) {
+						if($numrows==0) {
+							//we failed to update the user, log it as an error
+							$this->addLog($nfpt_user->email, 'SYNC', 'ERROR', 'Processed user successfully but failed to update users_live table afterwards. Something isn\'t right here.');
+						} else {
+							//we updated more than 1 user, log it as an error
+							$this->addLog($nfpt_user->email, 'SYNC', 'ERROR', 'Processed user successfully, but accidentally updated '.$numrows.' rows in the users_live table afterwards. Should have only updated 1 row. Something isn\'t right here.');
+						}
+					}
+				}
+			}
+		}
+
+		//before we end, we want to set error handling back to the regular Joomla method
+		JError::setErrorHandling(E_WARNING, 'message');
+		JError::setErrorHandling(E_NOTICE, 'message');
+		return true;
+	}
+
+	/*
+	 * Helper function to find a user given a set of possible field/values to search
+	 *
+	 * @param fieldarray should be an array. Each element in that array should be an associative array with a key/value pair
+	 */
+	private function fetch_juser_from_fields($fieldarray) {
+		$db = JFactory::getDbo();
+		foreach($fieldarray as $field) {
+			$query = "SELECT id FROM #__users WHERE {$field['key']}=".$db->quote($field['value']);
+			$db->setQuery($query);
+			$db->query();
+			if($db->getNumRows() == 1) {
+				return JUser::getInstance($db->loadResult());
+			}
+		}
+		return false;
+	}
+
+	private function db_connect() {
+		//if we have already connected, we do not need to connect again
+		if($this->nfpt_db) return true;
+
+		//otherwise, connect using the information in the plugin parameters
+		$nfpt_options = array(
+			'host' => $this->params->get('mainhost'),
+			'user' => $this->params->get('mainuser'),
+			'password' => $this->params->get('mainpass'),
+			'database' => $this->params->get('maindb'),
+		);
+		$this->nfpt_db = JDatabase::getInstance($nfpt_options);
+
+		//return whether or not we were able to connect to the database
+		if($this->nfpt_db instanceof JException) {
+			return $this->nfpt_db = false;
+		}
+		return !$this->nfpt_db->getErrorNum();
+	}
+
+	private function addlog($email, $operation, $status='SUCCESS', $notes = '') {
+		$origin=JURI::base();
+		$query = 'INSERT INTO userlog(origin, email, operation, status, notes) VALUES ('.$this->nfpt_db->quote($origin).','.$this->nfpt_db->quote($email).','.$this->nfpt_db->quote($operation).','.$this->nfpt_db->quote($status).','.$this->nfpt_db->quote($notes).')';
+		$this->nfpt_db->setQuery($query);
+		$this->nfpt_db->query();
+		if($this->nfpt_db->getErrorNum()) {
+			$mailer = JFactory::getMailer();
+			$config = JFactory::getConfig();
+			$site = JURI::root();
+			$message = <<<HEREDOC
+Failed to insert userlog entry into the database from $site after processing user.
+The user's email address is '$email'.
+The attempted operation was '$operation', which terminiated with status '$status'.
+
+NOTES (if applicable):
+$notes
+
+If you receive this email, there is something fishy going on - please contact tech@mangotreemedia.com for assistance.
+HEREDOC;
+			$mailer->addRecipient($this->params->get('failemail'));
+			$mailer->setSender($config->getValue('config.mailfrom'), $config->getValue('config.fromname'));
+			$mailer->setSubject('Userlog Failure');
+			$mailer->setBody($message);
+			if(!$mailer->Send()) {
+				throw new Exception('There is an error in the user application. Please contact tech@mangotreemedia.com if you continue to experience this issue.');
+			}
+		}
+	}
+
+	function handleWarning(&$error, $options=array()) {
+		$this->addlog('', 'SYNC', 'WARNING', $error->get('message'));
+		return $error;
+	}
+	function handleNotice(&$error, $options=array()) {
+		$this->addlog('', 'SYNC', 'NOTICE', $error->get('message'));
+		return $error;
+	}
+}
